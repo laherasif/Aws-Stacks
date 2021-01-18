e4c787e4894719e3cb76da7cb460481db02a9c53
laherasif <laherasif@gmail.com>
2021-01-18 21:42:10 +0500

    remove errors

--- lib/step00_hello_cdk-stack.ts
+++ lib/step00_hello_cdk-stack.ts
@@ -1,7 +1,7 @@
 import * as cdk from '@aws-cdk/core';
 import * as s3 from '@aws-cdk/aws-s3';
 
-export class Step00HelloCdkStack extends cdk.Stack {
+export class MyCdkStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
